e4dcc29fb14b4c35791f990793b19b20bf4e19bd
Saad <saadabouelsooud@gmail.com>
2021-04-07 10:56:46 +0200

    ya kareem

--- Sources/HiveFramework/Common/Views/SubmitButtonView.swift
+++ Sources/HiveFramework/Common/Views/SubmitButtonView.swift
@@ -44,7 +44,7 @@ struct SubmitButtonView: View {
                     .foregroundColor(Color(hex: canSubmit ? (submitButtonStyle?.fontColor!)! : (submitButtonStyle?.hoverFontColor!)!))
                     .fontWeight((submitButtonStyle?.fontBold!)! ? .bold : .none)
                     .multilineTextAlignment(.center)
-                    .padding(.horizontal,-20)
+                    .padding(.horizontal,-25)
             }
         })
         .disabled(!canSubmit)
